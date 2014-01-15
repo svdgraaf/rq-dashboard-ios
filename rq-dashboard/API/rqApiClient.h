35b2654e4aae87ee36c8e24a7f82b1905e6a3506
Sander van de Graaf <mail@svdgraaf.nl>
2014-01-15 19:48:29 +0100

    initial import

---
+++ rq-dashboard/API/rqApiClient.h
@@ -0,0 +1,13 @@
+//
+//  rqApiClient.h
+//  rq-dashboard
+//
+//  Created by Sander van de Graaf on 1/15/14.
+//  Copyright (c) 2014 Serinus42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rqApiClient : AFHTTPClient
+
+@end
---
+++ rq-dashboard/API/rqApiClient.m
@@ -0,0 +1,13 @@
+//
+//  rqApiClient.m
+//  rq-dashboard
+//
+//  Created by Sander van de Graaf on 1/15/14.
+//  Copyright (c) 2014 Serinus42. All rights reserved.
+//
+
+#import "rqApiClient.h"
+
+@implementation rqApiClient
+
+@end
